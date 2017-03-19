fc76d5d7ea255649f0a64cdaf51563f48d1a9bcd
Anton (home) <diz.vara@gmail.com>
2017-03-19 20:25:20 +0300

    motion blur

--- P2.py
+++ P2.py
@@ -164,10 +164,18 @@ for i in range(n_classes):
 
 import math
 
+def getMotionKernel(size):
+    kernel = np.zeros((size, size));
+    kernel[int((size-1)/2), :] = np.ones(size)/size;
+    return kernel
+    
+motion_kern3 = getMotionKernel(3);
+motion_kern5 = getMotionKernel(5);    
+    
 def getPerspMatrix(x, y, z, size):
-    w, h = size
-    half_w = w/2.
-    half_h = h/2.
+    w, h = size;
+    half_w = w/2.;
+    half_h = h/2.;
 
     
     rx = math.radians(x);
@@ -193,9 +201,9 @@ def getPerspMatrix(x, y, z, size):
             [-cos_y * sin_z, cos_z * cos_x - sin_z * sin_y * sin_x],
             [sin_y,          cos_y * sin_x],
         ]
-    )
+    );
 
-    center = np.float32([half_h, half_w])
+    center = np.float32([half_h, half_w]);
     offset = np.float32(
         [
             [-half_w, -half_h],
@@ -203,16 +211,16 @@ def getPerspMatrix(x, y, z, size):
             [ half_w,  half_h],
             [-half_w,  half_h],
         ]
-    )
+    );
 
-    points_z = np.dot(offset, R[2])
-    dev_z = np.vstack([w/(w + points_z), h/(h + points_z)])
+    points_z = np.dot(offset, R[2]);
+    dev_z = np.vstack([w/(w + points_z), h/(h + points_z)]);
 
-    new_points = np.dot(offset, R[:2].T) * dev_z.T + center
-    in_pt = np.float32([[0, 0], [w, 0], [w, h], [0, h]])
+    new_points = np.dot(offset, R[:2].T) * dev_z.T + center;
+    in_pt = np.float32([[0, 0], [w, 0], [w, h], [0, h]]);
 
-    transform = cv2.getPerspectiveTransform(in_pt, new_points)
-    return transform
+    transform = cv2.getPerspectiveTransform(in_pt, new_points);
+    return transform;
 
 
 
@@ -237,11 +245,18 @@ def augmentImage(img, N:int):
     rangeZ = [-15, 15];
 
 
-    for i in range(N):
+    for i in range(N-1):
         x = np.random.uniform(rangeX[0], rangeX[1]);
         y = np.random.uniform(rangeY[0], rangeY[1]);
         z = np.random.uniform(rangeZ[0], rangeZ[1]);
-        out.append(transformImg(img,x,y,z));
+        motion = np.random.uniform();
+        if motion > 0.9:
+            tmp = cv2.filter2D(img,-1,motion_kern5);
+        elif motion > 0.6 :
+            tmp = cv2.filter2D(img,-1,motion_kern3);
+        else:
+            tmp = img;
+        out.append(transformImg(tmp,x,y,z));
     return out;
 #%%
 
