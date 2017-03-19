345abc669b4adae854cb97081a8b4239017fd470
diz@ub14 <dizvara@gmail.com>
2017-03-19 20:14:42 +0300

    changes in the net

--- MixNet.py
+++ MixNet.py
@@ -22,15 +22,15 @@ def MixNet(x):
     # convolutional and pooling layers.
     
     #32x32x3 -> 30x30x8
-    w11 = tf.Variable(tf.truncated_normal((3,3,3,8),0,0.01))
-    b11 = tf.Variable(tf.truncated_normal([8],0,0.001))
+    w11 = tf.Variable(tf.truncated_normal((3,3,3,4),0,0.1))
+    b11 = tf.Variable(tf.truncated_normal([4],0,0.001))
 
     c1 = tf.nn.conv2d(x,w11, strides = [1,1,1,1], padding='VALID') + b11
     c1 = tf.nn.relu(c1)
     
     #30x30x8 -> 28x28x8
-    w12 = tf.Variable(tf.truncated_normal((3,3,8,8),0,0.01))
-    b12 = tf.Variable(tf.truncated_normal([8],0,0.001))
+    w12 = tf.Variable(tf.truncated_normal((3,3,4,16),0,0.1))
+    b12 = tf.Variable(tf.truncated_normal([16],0,0.001))
     
     c1 = tf.nn.conv2d(c1,w12, strides = [1,1,1,1], padding='VALID') + b12
     
@@ -43,15 +43,15 @@ def MixNet(x):
     print("layer1 :",c1.get_shape(),"; flattened=", flat1.get_shape())
     
     #14x14x8 -> 12x12x16
-    w21 = tf.Variable(tf.truncated_normal((3,3,8,16),0,0.01))
+    w21 = tf.Variable(tf.truncated_normal((3,3,16,16),0,0.1))
     b21 = tf.Variable(tf.truncated_normal([16],0,0.001))
 
     c2 = tf.nn.conv2d(c1,w21, strides = [1,1,1,1], padding='VALID') + b21
     c2 = tf.nn.relu(c2)
     
     #12x12x16 -> 10x10x32
-    w22 = tf.Variable(tf.truncated_normal((3,3,16,32),0,0.01))
-    b22 = tf.Variable(tf.truncated_normal([32],0,0.001))
+    w22 = tf.Variable(tf.truncated_normal((3,3,16,64),0,0.1))
+    b22 = tf.Variable(tf.truncated_normal([64],0,0.01))
     
     c2 = tf.nn.conv2d(c2,w22, strides = [1,1,1,1], padding='VALID') + b22
     
@@ -64,31 +64,34 @@ def MixNet(x):
     
 
 
-    #5x5x32 -> 3x3x64
-    w31 = tf.Variable(tf.truncated_normal((3,3,32,64),0,0.01))
-    b31 = tf.Variable(tf.truncated_normal([64],0,0.001))
+    #5x5x64 -> 3x3x64
+    w31 = tf.Variable(tf.truncated_normal((3,3,64,128),0,0.1))
+    b31 = tf.Variable(tf.truncated_normal([128],0,0.01))
 
     c3 = tf.nn.conv2d(c2,w31, strides = [1,1,1,1], padding='VALID') + b31
     c3 = tf.nn.relu(c3)
     
-    #3x3x64 -> 1x1x128
-    w32 = tf.Variable(tf.truncated_normal((3,3,64,128),0,0.01))
-    b32 = tf.Variable(tf.truncated_normal([128],0,0.001))
+    #3x3x64 -> 1x1x512
+    w32 = tf.Variable(tf.truncated_normal((3,3,128,512),0,0.1))
+    b32 = tf.Variable(tf.truncated_normal([512],0,0.01))
 
     c3 = tf.nn.conv2d(c3,w32, strides = [1,1,1,1], padding='VALID') + b32
     c3 = tf.nn.relu(c3)
-    #1X128 -> 128
+    #1X128 -> 512
     flat3 = flatten(c3);    
     print("layer3 :",c3.get_shape(),"; flattened=", flat3.get_shape())
     
    
     #1568+800+128
-    lin1 = tf.concat([flat1, flat2,flat3], 1)
-    print("lin1 shape:",lin1.get_shape())
+    lin1 = tf.concat([flat2,flat3], 1)
+    lin1len = int(lin1.get_shape()[1]);
+    print("lin1 shape:",lin1.get_shape(), lin1len)
 
-    wl1 = tf.Variable(tf.truncated_normal((1568+800+128,256),0,0.01))
+    #1568+800+128
+    wl1 = tf.Variable(tf.truncated_normal((lin1len,256),0,0.01))
     bl1 = tf.Variable(tf.truncated_normal([256],0,0.001))
 
+    lin1 = tf.nn.dropout(lin1, keep_prob)
     lin1 = tf.matmul(lin1,wl1) + bl1
     lin1 = tf.nn.relu(lin1)
     
