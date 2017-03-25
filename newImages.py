323388a434155c230ce1df41141624cf8da9cef7
Anton (home) <diz.vara@gmail.com>
2017-03-25 09:17:03 +0300

    newImages

---
+++ newImages.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 24 22:46:21 2017
+
+@author: diz
+"""
+
+import os
+
+newImages = []
+for entry in os.scandir('../Data/Signs/t'):
+    if entry.is_file():
+        img = cv2.imread(entry.path)
+        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        img = cv2.resize(img,(32,32))
+        newImages.append(img)
+        
+newImages = np.array(newImages)
+        
+cols = 5
+
+figsize = (10, 5)
+nImages = newImages.shape[0]
+
+gs = gridspec.GridSpec(nImages // cols + 1, cols)
+
+fig1 = plt.figure(num=1, figsize=figsize)
+ax = []
+for i in range(nImages):
+    row = (i // cols)
+    col = i % cols
+    ax.append(fig1.add_subplot(gs[row, col]))
+    #example
+    img = newImages[i]
+    #rescale to make dark images visible
+    ax[-1].imshow(img)
+    ax[-1].axis('off')
+
+        
