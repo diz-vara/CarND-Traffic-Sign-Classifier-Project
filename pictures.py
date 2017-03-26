4d2418b386af19fd9143b74448b49954b58fe6ed
Anton (home) <diz.vara@gmail.com>
2017-03-26 13:35:24 +0300

    pictures

--- pictures.py
+++ pictures.py
@@ -31,6 +31,9 @@ for i in range(n_classes):
     ax[-1].axis('off')
     
 #%%
+
+image = X_train[classIndicies[4][40]] #sl70    
+#%%
 figsize = (20, 30)
 gs = gridspec.GridSpec(1, 3)
 
@@ -52,6 +55,7 @@ ax[-1].imshow(img_m5)
 ax[-1].axis('off')
     
 #%%
+#rotations
 figsize = (20, 10)
 gs = gridspec.GridSpec(2, 3)
 
@@ -88,6 +92,53 @@ ax[-1].imshow(transformImg(img,20,15,15))
 ax[-1].axis('off')
 
 #%%
+#scale
+figsize = (10, 10)
+gs = gridspec.GridSpec(1, 3)
+
+fig1 = plt.figure(num=2, figsize=figsize)
+ax = []
+ax.append(fig1.add_subplot(gs[0, 0]))
+ax[-1].set_title('Original image')
+ax[-1].imshow(img)
+ax[-1].axis('off')
+
+
+ax.append(fig1.add_subplot(gs[0, 1]))
+ax[-1].set_title('scale 0.8')
+ax[-1].imshow(transformImg(img,0,0,0,0.8))
+ax[-1].axis('off')
+
+ax.append(fig1.add_subplot(gs[0, 2]))
+ax[-1].set_title('scale 1.2')
+ax[-1].imshow(transformImg(img,0,0,0,1.2))
+ax[-1].axis('off')
+#%%
+#light
+figsize = (10, 10)
+gs = gridspec.GridSpec(1, 3)
+
+fig1 = plt.figure(num=2, figsize=figsize)
+ax = []
+ax.append(fig1.add_subplot(gs[0, 0]))
+ax[-1].set_title('Original image')
+ax[-1].imshow(img)
+ax[-1].axis('off')
+
+tmp=normalizeImageG(img)
+
+ax.append(fig1.add_subplot(gs[0, 1]))
+ax[-1].set_title('lightened')
+ax[-1].imshow(np.clip(tmp+0.3,-0.5,0.5)+0.5)
+ax[-1].axis('off')
+
+ax.append(fig1.add_subplot(gs[0, 2]))
+ax[-1].set_title('darkened')
+ax[-1].imshow(np.clip(tmp-0.3,-0.5,0.5)+0.5)
+ax[-1].axis('off')
+
+#%%
+#errors
 figsize = (20, 10)
 gs = gridspec.GridSpec(1, 7)
 
@@ -100,5 +151,4 @@ for i in range(7):
     ax[-1].set_title('')
     ax[-1].imshow(X_valid[e[i+7]])
     ax[-1].axis('off')
-    
-    
+
